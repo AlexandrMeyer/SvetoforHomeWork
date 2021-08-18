1ffe9a823d60f06f19ee69d00eacbf5b272991f2
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-08-18 16:23:52 +0300

    lastCorrection

--- SvetoforHomeWork/ViewController.swift
+++ SvetoforHomeWork/ViewController.swift
@@ -14,40 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet var greenView: UIView!
     @IBOutlet var startButton: UIButton!
     
+    let lightOff = CGFloat(0.3)
+    let lightOn = CGFloat(1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redView.alpha = 0.3
+        redView.alpha = lightOff
         redView.layer.cornerRadius = redView.frame.width / 2
         
-        yellowView.alpha = 0.3
+        yellowView.alpha = lightOff
         yellowView.layer.cornerRadius = yellowView.frame.width / 2
         
-        greenView.alpha = 0.3
+        greenView.alpha = lightOff
         greenView.layer.cornerRadius = greenView.frame.width / 2
         
         startButton.layer.cornerRadius = 10
-        startButton.setTitle("START", for: .normal)
     }
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         startButton.setTitle("NEXT", for: .normal)
         
-        if redView.alpha == yellowView.alpha && redView.alpha == greenView.alpha{
-            redView.alpha = 1
-            yellowView.alpha = 0.3
-            greenView.alpha = 0.3
+        if redView.alpha == yellowView.alpha && redView.alpha == greenView.alpha {
+            redView.alpha = lightOn
+            yellowView.alpha = lightOff
+            greenView.alpha = lightOff
         } else if redView.alpha > yellowView.alpha {
-            redView.alpha = 0.3
-            yellowView.alpha = 1
-            greenView.alpha = 0.3
+            redView.alpha = lightOff
+            yellowView.alpha = lightOn
+            greenView.alpha = lightOff
         } else if yellowView.alpha > greenView.alpha {
-            redView.alpha = 0.3
-            yellowView.alpha = 0.3
-            greenView.alpha = 1
+            redView.alpha = lightOff
+            yellowView.alpha = lightOff
+            greenView.alpha = lightOn
         } else {
-            redView.alpha = 1
-            yellowView.alpha = 0.3
-            greenView.alpha = 0.3
+            redView.alpha = lightOn
+            yellowView.alpha = lightOff
+            greenView.alpha = lightOff
         }
     }
 }
